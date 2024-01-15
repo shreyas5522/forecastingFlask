f27720f74137e69f24635bf019c9b2f946784acb
Shreyash Jagtap <shreyash5522@gmail.com>
2024-01-15 23:27:06 +0530

    15-Jan-2024 | Support for Login

--- app.py
+++ app.py
@@ -1,24 +1,108 @@
 # app.py
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, validators
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'generated-secret-key'  # Change this to a random secret key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://phpmyadmin:admin%40123@localhost/login'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# Create User model
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
+# Create a form for login
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[validators.DataRequired()])
+    password = PasswordField('Password', validators=[validators.DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+# Create a form for signup
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[validators.DataRequired(), validators.Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[validators.DataRequired(), validators.Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[validators.DataRequired(), validators.EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
+@app.route('/forecast')
+def home():
+    if current_user.is_authenticated:
+        # User is logged in, display welcome message
+        return render_template('forecast.html', username=current_user.username)
+    else:
+        # User is not logged in, redirect to login page
+        flash('Please log in to access the home page.', 'info')
+        return redirect(url_for('login'))
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    name = request.args.get("name", "World")
-    return render_template('login.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            login_user(user, remember=form.remember.data)
+            flash('Login successful!', 'success')
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Check your username and password.', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created successfully. You can now log in!', 'success')
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('login'))
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-@app.route('/forecast')
-def forecast():
-    return render_template('forecast.html')
-
-@app.route('/login2')
-def login2():
-    return render_template('login2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
